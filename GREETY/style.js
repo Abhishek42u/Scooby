c7e80ac20b566764e3eec595250981ddee618d67
Rohit Shaw <carpediem212045@gmail.com>
2022-10-12 15:13:11 +0530

    Merged Drag into it

---
+++ GREETY/style.js
@@ -0,0 +1,19 @@
+function dragStart() {
+    currTile = this; //this refers to the img tile being dragged
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to the img tile being dropped on
+}
